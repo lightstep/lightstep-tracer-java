b2e87113fd4b232b061c5c524619c62611158857
bcronin <bcronin@resonancelabs.com>
2016-04-26 14:31:00 -0700

    Simplfy build steps for sample

---
+++ samples/jre-simple/README.md
@@ -0,0 +1,11 @@
+# jre-simple
+
+A simple "Hello World"-esque sample using the LightStep JRE library.
+
+## Usage
+
+```
+make build      # build the sample
+make run        # builds and runs the example
+make clean      # clean up generated files
+```
--- samples/jre-simple/src/main/java/com/lightstep/tracer/samples/jre/Simple.java
+++ samples/jre-simple/src/main/java/com/lightstep/tracer/samples/jre/Simple.java
@@ -5,12 +5,14 @@ import com.lightstep.tracer.Span;
 
 public class Simple {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Starting Simple example...");
 
         Tracer tracer = new com.lightstep.tracer.jre.JreTracer(
             new com.lightstep.tracer.shared.Options("{your_access_token}"));
 
         Span span = tracer.buildSpan("test_span").start();
         span.finish();
+
+        System.out.println("Done!");
     }
 }
