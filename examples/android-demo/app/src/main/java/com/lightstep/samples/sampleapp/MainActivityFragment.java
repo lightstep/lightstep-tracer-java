b3c9717ef5c264f9bf79e1c81bb4023d058ad104
bcronin <bcronin@lightstep.com>
2016-07-07 12:11:37 -0700

    Remove commented out code

--- examples/android-demo/app/src/main/java/com/lightstep/samples/sampleapp/MainActivityFragment.java
+++ examples/android-demo/app/src/main/java/com/lightstep/samples/sampleapp/MainActivityFragment.java
@@ -20,7 +20,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import io.opentracing.Span;
-//import com.lightstep.tracer.android.Tracer;
 import io.opentracing.Tracer;
 
 import org.json.JSONArray;
