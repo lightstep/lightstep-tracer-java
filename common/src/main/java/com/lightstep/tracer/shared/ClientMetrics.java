57aed28d901b540ac367ead80a3502b7467a7b09
bcronin <bcronin@lightstep.com>
2016-07-13 17:26:31 -0700

    Add a spans.dropped count to the Java client

---
+++ common/src/main/java/com/lightstep/tracer/shared/ClientMetrics.java
@@ -0,0 +1,32 @@
+package com.lightstep.tracer.shared;
+
+import java.util.ArrayList;
+import com.lightstep.tracer.thrift.Metrics;
+import com.lightstep.tracer.thrift.MetricsSample;
+
+public class ClientMetrics {
+
+    // For capacity allocation purposes, keep this in sync with the number of
+    // counts actually being tracked.
+    private final int NUMBER_OF_COUNTS = 1;
+
+    public long spansDropped;
+
+    public ClientMetrics() {
+        this.spansDropped = 0;
+    }
+    
+    public ClientMetrics(ClientMetrics that) {
+        this.spansDropped = that.spansDropped;
+    }
+
+    public void merge(ClientMetrics that) {
+        this.spansDropped += that.spansDropped;
+    }
+
+    public Metrics toThrift() {
+        ArrayList<MetricsSample> counts = new ArrayList<MetricsSample>(NUMBER_OF_COUNTS);
+        counts.add(new MetricsSample("spans.dropped").setInt64_value(spansDropped));
+        return new Metrics().setCounts(counts);
+    }
+}
