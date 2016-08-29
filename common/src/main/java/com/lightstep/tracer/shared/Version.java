5d64e5333ef2dca744c4f8090add0381be99fbe0
bcronin <bcronin@lightstep.com>
2016-08-29 16:27:38 -0700

    Update VERSION

--- common/src/main/java/com/lightstep/tracer/shared/Version.java
+++ common/src/main/java/com/lightstep/tracer/shared/Version.java
@@ -2,7 +2,7 @@
 package com.lightstep.tracer.shared;
 
 public class Version {
-  public static final String LIGHTSTEP_TRACER_VERSION = "0.9.6";
+  public static final String LIGHTSTEP_TRACER_VERSION = "0.9.7";
 }
 
 
