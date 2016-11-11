490573ddae304b15e7e45acc200ea1f934666bfb
Sarah Haskins <sarahhaskins@gmail.com>
2016-11-10 16:34:34 -0800

    Remove static final modifiers from interface
    They are redundant.

--- common/src/main/java/com/lightstep/tracer/shared/Propagator.java
+++ common/src/main/java/com/lightstep/tracer/shared/Propagator.java
@@ -13,7 +13,7 @@ public interface Propagator<C> {
 
 
     // The three supported Propagators.
-    static final Propagator<TextMap> TEXT_MAP = new Propagator<TextMap>() {
+    Propagator<TextMap> TEXT_MAP = new Propagator<TextMap>() {
         private static final String PREFIX_TRACER_STATE = "ot-tracer-";
         private static final String PREFIX_BAGGAGE = "ot-baggage-";
 
@@ -68,7 +68,7 @@ public interface Propagator<C> {
     // TODO: HTTP_HEADERS presently blindly delegates to
     // TEXT_MAP; adopt BasicTracer's HTTP carrier encoding once it's
     // been defined.
-    static final Propagator<TextMap> HTTP_HEADERS = new Propagator<TextMap>() {
+    Propagator<TextMap> HTTP_HEADERS = new Propagator<TextMap>() {
         public void inject(SpanContext spanContext, TextMap carrier) {
             TEXT_MAP.inject(spanContext, carrier);
         }
@@ -78,7 +78,7 @@ public interface Propagator<C> {
         }
     };
 
-    static final Propagator<ByteBuffer> BINARY = new Propagator<ByteBuffer>() {
+    Propagator<ByteBuffer> BINARY = new Propagator<ByteBuffer>() {
         public void inject(SpanContext spanContext, ByteBuffer carrier) {
             // TODO: implement
         }
