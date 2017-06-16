687b2948b29d1b10f4d0e090be579ef0e190ffe7
Daniela Miao <dym@lightstep.com>
2017-06-15 17:27:04 -0700

    VERSION 0.12.4

--- common/src/main/java/com/lightstep/tracer/shared/Version.java
+++ common/src/main/java/com/lightstep/tracer/shared/Version.java
@@ -2,5 +2,5 @@
 package com.lightstep.tracer.shared;
 
 public class Version {
-  public static final String LIGHTSTEP_TRACER_VERSION = "0.12.3";
+  public static final String LIGHTSTEP_TRACER_VERSION = "0.12.4";
 }
