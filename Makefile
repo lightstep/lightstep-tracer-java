87990ab9c087170701116356c0379878116f3617
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2020-07-02 01:53:55 +0200

    Initial support for release-on-tag. (#250)

--- Makefile
+++ Makefile
@@ -15,8 +15,13 @@ ci_build:
 ci_test:
 	mvn exec:java -pl examples
 
+# Needed only in CircleCI to FORCE the help plugin to be installed (resolve-plugins is NOT enough).
+resolve_plugins:
+	mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version
+
+# If no version is specified, the minor version will be automatically increased.
 inc-version:
-	./inc-version.sh
+	./inc-version.sh $(NEW_VERSION)
 
 # See https://bintray.com/lightstep for published artifacts
 # You must have the following entry in your settings.xml of your .m2 directory
@@ -28,7 +33,7 @@ inc-version:
 #        <password>xxx</password>
 #    </server>
 #
-publish: build
+publish: build resolve_plugins
 	@test -n "$$BINTRAY_USER" || (echo "BINTRAY_USER must be defined to publish" && false)
 	@test -n "$$BINTRAY_API_KEY" || (echo "BINTRAY_API_KEY must be defined to publish" && false)
 	@test -n "$$MAVEN_CENTRAL_USER_TOKEN" || (echo "MAVEN_CENTRAL_USER_TOKEN must be defined to publish" && false)
@@ -36,4 +41,4 @@ publish: build
 	@test -n "$$BINTRAY_GPG_PASSPHRASE" || (echo "$$BINTRAY_GPG_PASSPHRASE must be defined to publish" && false)
 
 	@git diff-index --quiet HEAD || (echo "git has uncommitted changes. Refusing to publish." && false)
-	./tag-and-deploy.sh
+	./deploy.sh
--- inc-version.sh
+++ inc-version.sh
@@ -3,14 +3,22 @@
 # To increment the version, use the Makefile
 # make publish
 # which will call out to this script
+set -e
 
-CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ "$#" -lt 1 ]; then
+        echo "Increasing minor version automatically"
 
-CURRENT_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+        # Use maven-help-plugin to get the current project.version
+        CURRENT_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+        # Increment the minor version
+        NEW_VERSION="${CURRENT_VERSION%.*}.$((${CURRENT_VERSION##*.}+1))"
+else
+	NEW_VERSION=$1
+fi
 
-# Increment the minor version
-NEW_VERSION="${CURRENT_VERSION%.*}.$((${CURRENT_VERSION##*.}+1))"
-export NEW_VERSION
+# Create a branch with the version bump.
+NEW_VERSION_BRANCH="v${NEW_VERSION}_bump"
+git checkout -b $NEW_VERSION_BRANCH
 
 # Use maven-help-plugin to update the project.version
 mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false
@@ -29,4 +37,4 @@ git add shadow/pom.xml
 git add lightstep-tracer-jre/src/main/java/com/lightstep/tracer/jre/Version.java
 
 git commit -m "VERSION $NEW_VERSION"
-git push --set-upstream origin $CURRENT_BRANCH
+git push --set-upstream origin $NEW_VERSION_BRANCH
